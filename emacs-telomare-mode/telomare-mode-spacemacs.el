c28ba9c7ce9c672e0b4e685180e7dd7ad3f220e3
hhefesto <hhefesto@rdataa.com>
2025-12-12 11:12:17 -0600

    function returning telomare-mode failed

--- emacs-telomare-mode/telomare-mode-spacemacs.el
+++ emacs-telomare-mode/telomare-mode-spacemacs.el
@@ -1,52 +1,35 @@
-;; Define Telomare mode
-(define-derived-mode telomare-mode prog-mode "Telomare"
-  "Major mode for editing Telomare files."
-  (setq-local comment-start "-- ")
-  (setq-local comment-end ""))
+(defun telomare-setup (project-path)
+  "Set up Telomare major mode and LSP using PROJECT-PATH."
 
-;; Associate .tel files with telomare-mode
-(add-to-list 'auto-mode-alist '("\\.tel\\'" . telomare-mode))
+  (message "telomare-setup called")
 
+  ;; --- Major mode ---
+  (define-derived-mode telomare-mode prog-mode "Telomare"
+    "Major mode for editing Telomare files."
+    (setq-local comment-start "-- ")
+    (setq-local comment-end ""))
 
-;; Configure LSP for Telomare
-(with-eval-after-load 'lsp-mode
-  ;; Register the Telomare language server
-  (lsp-register-client
-   (make-lsp-client
-    :new-connection (lsp-stdio-connection '("path/to/your/telomare-lsp-exe"))
-    :activation-fn (lsp-activate-on "telomare")
-    :server-id 'telomare-lsp
-    :priority -1
-    :initialization-options '()
-    :notification-handlers (lsp-ht)
-    :request-handlers (lsp-ht)
-    :completion-in-comments? t))
+  ;; --- File association ---
+  (add-to-list 'auto-mode-alist '("\\.tel\\'" . telomare-mode))
 
+  ;; --- Ensure lsp-mode is loaded ---
+  (require 'lsp-mode)
+
+  ;; --- Language ID ---
   (add-to-list 'lsp-language-id-configuration
                '(telomare-mode . "telomare"))
 
-  )
-
-;; Configure LSP for Telomare
-(with-eval-after-load 'lsp-mode
-  ;; Enable semantic tokens
-  (setq lsp-semantic-tokens-enable t)
-
-  (add-to-list 'lsp-language-id-configuration '(telomare-mode . "telomare"))
-
+  ;; --- Register LSP client ---
   (lsp-register-client
    (make-lsp-client
-    :new-connection (lsp-stdio-connection
-                     (lambda ()
-                       (list "nix" "run" "/home/hhefesto/src/telomare#lsp" "--")))
-    :activation-fn (lsp-activate-on "telomare")
+    :server-id 'telomare-lsp
     :major-modes '(telomare-mode)
-    :server-id 'telomare-lsp))
-  (add-to-list 'lsp-language-id-configuration
-               '(telomare-mode . "telomare")))
+    :command-fn
+    (lambda ()
+      (message "🚀 Launching Telomare LSP")
+      (list "nix" "run" (concat project-path "#lsp") "--"))))
 
-;; Auto-start LSP in telomare-mode
-(add-hook 'telomare-mode-hook #'lsp)
+  ;; --- Auto-start LSP ---
+  (add-hook 'telomare-mode-hook #'lsp-deferred))
 
-(provide 'telomare-mode-spacemacs)
-;;; telomare-config.el ends here
+(provide 'telomare-config)
